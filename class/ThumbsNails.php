7840263953795aced4a93eb57d78de00a1acf2ad
mambax7 <mambax7@gmail.com>
2020-08-13 12:21:58 -0400

    'mkdir(...)' race condition

--- class/ThumbsNails.php
+++ class/ThumbsNails.php
@@ -93,7 +93,7 @@ class ThumbsNails
         $path_to_check = XOOPS_ROOT_PATH . "/$img_path/$img_savepath";
 
         if (!\is_dir($path_to_check)) {
-            if (false === \mkdir($path_to_check, 0777)) {
+            if (!mkdir($path_to_check, 0777) && !is_dir($path_to_check)) {
                 //                return false;
             }
         }
